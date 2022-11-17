0b9290bcab2cb05261ed963c4467a6ba0b63cddb
champix56 <desorbaix@free.fr>
2022-11-17 18:14:37 +0100

    skeleton object for form

---
+++ js/formulaire.js
@@ -0,0 +1,12 @@
+class VueFormulaire{
+    #vueHref='create.html';
+    domRefElement=undefined;
+    meme=undefined;
+    constructor(){
+
+    }
+    #addEvents=()=>{}
+    loadPage=(domRefWrapper)=>{}
+    changeMemeValue=(partialMemeData,evt)=>{}
+    onsubmitForm=(evt)=>{}
+}
