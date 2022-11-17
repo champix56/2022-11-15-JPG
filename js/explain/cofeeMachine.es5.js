56043bb81b79fd558a428ac89c87a00b13ee2835
champix56 <desorbaix@free.fr>
2022-11-17 14:52:24 +0100

    gestion objet par function es5 + arrow function

--- js/explain/cofeeMachine.es5.js
+++ js/explain/cofeeMachine.es5.js
@@ -14,8 +14,14 @@ function MachineACafee(volumeBacAEau,volumeCafeMax){
     this.niveauEau=0;
     this.volumeCafeCoule=0;
     this.volumePoudreCafe=0;
+    this.cleanMachine=function(){
+        console.log('netoyyage de la machine', this);
+       this.niveauEau=0;
+       this.volumePoudreCafe=0;
+        this.volumeCafeCoule=0;
 
-    this.allumage=function(){
+    }
+    this.allumage=()=>{
         console.log('allumage general de la machine', this);
         allumageResistance();
         ///attente
@@ -36,3 +42,40 @@ machine.niveauEau=1.0;
 
 //execution de fonction public
 machine.allumage();
+
+
+function Cuisine(){
+   
+    this.coffeeMachine = null;
+    this.oven = null;
+    
+    this.netoyage=null;
+    this.routineMatinal=null;
+}
+
+var cuisine= new Cuisine();
+cuisine.coffeeMachine=machine;
+
+
+//pas besoin de binding pour la 
+//delegation de function car la declaration est effectuée par arrow function
+cuisine.routineMatinal=machine.allumage
+
+//declaration de la fonction interne grace au mot clef function 
+//necessite de binder le context pour que le this de la function 
+//reste bien la machine (possibilité de binder autre chose dans le this)
+cuisine.netoyage=(machine.cleanMachine).bind(machine);
+
+
+
+console.log('%c%s','color:red;font-size:32pt','passage à l\'objet cuisine');
+//il est 8h 
+cuisine.routineMatinal();
+cuisine.netoyage();
+console.log(cuisine);
+
+//surcharge d'un type existant avec ajout 
+//a toutes les instances deja existante et les nouvelles instance 
+var chaine="chaine";
+String.prototype.sub2=function(){return this.substr(2)};
+chaine.sub2();
