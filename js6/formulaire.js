068c510ccf7b90d1c92e75261e3a13d1959a95a9
champix56 <desorbaix@free.fr>
2022-11-18 12:41:47 +0100

    ajout des options de select

--- js6/formulaire.js
+++ js6/formulaire.js
@@ -1,36 +1,42 @@
-import Images from './Objects/Images.js';
+import Images,{images} from './Objects/Images.js';
 import Image from './Objects/Image.js';
-import Meme from './Objects/Meme.js';
+import Meme,{currentMeme} from './Objects/Meme.js';
 
 import {loadPageByPromise} from './communFunctions.js'
-
-import {images, currentMeme} from './constantes.js'
+ 
 
 export default class VueFormulaire{
     #vueHref='create.html';
     domRefElement=undefined;
     meme=undefined;
     #nodeSelector=undefined;
+    #container=undefined;
     constructor(nodeSelector='#wrapper'){
         this.#nodeSelector=nodeSelector;
     }
     #addEvents=()=>{}
     #loadingContent=()=>{
-
+         const select= this.#container.querySelector('select')
+        images.map(e=>{
+            const o=document.createElement('option');
+            o.value=e.id;
+            o.innerHTML=e.titre;
+            select.append(o);
+        });
     }
     loadPage=(domRefWrapper)=>{
         const promiseImage=Images.fetch();
         const promiseHTMLTemplate=loadPageByPromise(this.#vueHref);
         Promise.all([promiseHTMLTemplate,promiseImage])
             .then(arrayRessources=>{
-                const container=arrayRessources[0];
-                images=arrayRessources[1];
-                
+                this.#container=arrayRessources[0];
+                images.replaceContentImagesArray(arrayRessources[1]);
+                console.log(images);
                 this.#loadingContent();
 
                 this.domRefElement=document.querySelector(this.#nodeSelector);
                 this.domRefElement.innerHTML='';
-                container.childNodes.forEach(elem=>this.domRefElement.append(elem));
+                this.#container.childNodes.forEach(elem=>this.domRefElement.append(elem));
             });
     }
     changeMemeValue=(partialMemeData,evt)=>{}
